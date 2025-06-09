6030d94dacd354a953470275fdcd0e44bca5ecf8
Guilherme Freitas <guilherme130108@gmail.com>
2025-06-09 14:45:45 -0300

    site base

--- projeto-3-main/script.js
+++
@@ -1,8 +0,0 @@
-let cartCount = 0;
-
-document.querySelectorAll('.add-to-cart').forEach(button => {
-  button.addEventListener('click', () => {
-    cartCount++;
-    document.getElementById('cart-count').innerText = cartCount;
-  });
-});
